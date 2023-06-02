4b916ada4b2168c72cff8543399858fee4be5a19
Rafael de Souza <rafael.souzalima@outlook.com>
2023-06-02 19:53:27 -0300

    Arquivo-base de implementação

---
+++ treap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct treap{
+    int info;
+    int chave;
+    struct treap *direita;
+    struct treap *esquerda;
+}treap;
+
+int main(){
+    treap *arvore = NULL;
+
+    
+
+    return 0;
+}
